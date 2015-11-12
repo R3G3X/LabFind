6477299cef72688c73397151901f9bf997294f80
KaitoHH <hh.kaito@gmail.com>
2015-11-12 21:20:58 +0800

    数据库更新

--- regex_db.sql
+++ regex_db.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `regex_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `regex_db`;
 -- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
 --
 -- Host: localhost    Database: regex_db
@@ -222,4 +220,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-11-12 20:50:24
+-- Dump completed on 2015-11-12 21:19:30
